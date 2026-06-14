bd017095eef98ee661b6760d9ad7505d529a3d38
geondongkim <geondongkim@gmail.com>
2026-06-14 23:39:38 +0900

    fix(api): SCRUM-8 wire push token service constructor (#141)

--- services/api-spring/src/main/java/com/onmu/api/service/PushTokenService.java
+++ services/api-spring/src/main/java/com/onmu/api/service/PushTokenService.java
@@ -14,6 +14,7 @@ import java.time.Instant;
 import java.util.HexFormat;
 import java.util.Set;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,6 +31,7 @@ public class PushTokenService {
   private final UserDeviceRepository userDeviceRepository;
   private final Clock clock;
 
+  @Autowired
   public PushTokenService(
     UserRepository userRepository,
     UserDeviceRepository userDeviceRepository
