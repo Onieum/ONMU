7599d933d1a78c554f69f52b93b9eb50c928041c
RudinP <song_e@sookmyung.ac.kr>
2026-06-15 13:04:44 +0900

    test(mobile): SCRUM-46 make time label tests timezone-safe

--- apps/mobile-flutter/test/group_repository_api_mapping_test.dart
+++ apps/mobile-flutter/test/group_repository_api_mapping_test.dart
@@ -230,7 +230,7 @@ void main() {
     );
     expect(messages[1].sender, 'ONMU');
     expect(messages[1].message, '새 투표가 열렸어요.');
-    expect(messages[1].timeLabel, '14:03');
+    expect(messages[1].timeLabel, _localTimeLabel('2026-06-09T14:03:00+09:00'));
     expect(messages[1].isMine, isFalse);
     expect(messages[2].sender, 'ONMU');
     expect(messages[2].message, '새 활동이 있어요.');
@@ -643,4 +643,11 @@ void main() {
       'http://127.0.0.1:8080/api/v1/media/public?key=dev%2Fmedia%2Frecords%2Fmemory-1004%2Fimage-1.jpg',
     );
   });
+}
+
+String _localTimeLabel(String value) {
+  final local = DateTime.parse(value).toLocal();
+  final hour = local.hour.toString().padLeft(2, '0');
+  final minute = local.minute.toString().padLeft(2, '0');
+  return '$hour:$minute';
 }
