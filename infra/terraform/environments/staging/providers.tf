c0452c90fb6b9117ae8d8d44318b6eb941cbeda4
geondongkim <geondongkim@gmail.com>
2026-06-16 22:21:31 +0900

    fix(infra): SCRUM-5 disable azurerm auto registration (#179)

--- infra/terraform/environments/staging/providers.tf
+++ infra/terraform/environments/staging/providers.tf
@@ -11,5 +11,9 @@ terraform {
 
 provider "azurerm" {
   subscription_id = var.subscription_id
+  # GitHub OIDC apply identity는 resource group scope 권한만 갖도록 유지한다.
+  # 필요한 Resource Provider는 운영자가 미리 등록한 상태를 전제로 하고,
+  # Terraform이 subscription scope에서 auto-registration을 시도하지 않게 막는다.
+  resource_provider_registrations = "none"
   features {}
 }
