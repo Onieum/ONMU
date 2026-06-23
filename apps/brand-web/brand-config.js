bf37d305873dd145a0daa0d59cad567598bc64b8
geondongkim <geondongkim@gmail.com>
2026-06-23 10:02:03 +0900

    [Feat] SCRUM-22 ONMU 브랜드 웹 완성 (#300)
    * feat(brand-web): SCRUM-22 add ONMU landing page
    
    * feat(brand-web): SCRUM-22 complete ONMU brand site
    
    * feat(brand-web): SCRUM-22 add observability deploy path
    
    * fix(brand-web): SCRUM-22 keep cloudflared deploy alive

---
+++ apps/brand-web/brand-config.js
@@ -0,0 +1,11 @@
+window.ONMU_BRAND_CONFIG = Object.assign(
+  {
+    sentryDsn: "",
+    sentryEnvironment: "production",
+    sentryRelease: "brand-web-static",
+    sentryErrorSampleRate: 1,
+    sentryTracesSampleRate: 0,
+    sentrySdkUrl: "https://browser.sentry-cdn.com/10.59.0/bundle.min.js"
+  },
+  window.ONMU_BRAND_CONFIG || {}
+);
